419a2f3335333eb39abb4f9b4cfc6d5dd1b0142b
wuxin402 <31025583+wuxin402@users.noreply.github.com>
2019-01-14 10:41:52 +0800

    Update PlatformClientUtils.java

--- wuxin-common-api/src/main/java/com/wuxin/utils/PlatformClientUtils.java
+++ wuxin-common-api/src/main/java/com/wuxin/utils/PlatformClientUtils.java
@@ -15,7 +15,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PlatformClientUtils {
+public class PlatformClientUtil {
 	private static final Logger logger = LoggerFactory.getLogger(PlatformClientUtils.class);
 
 	public static void close(Closeable x) {
